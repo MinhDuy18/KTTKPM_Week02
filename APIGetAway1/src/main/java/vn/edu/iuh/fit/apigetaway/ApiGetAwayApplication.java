31a76b8820370f512e83f39c07ef659d947b52ae
MinhDuy18 <duy19102018@gmail.com>
2024-01-23 22:48:31 +0700

    create APIGetAway

---
+++ APIGetAway1/src/main/java/vn/edu/iuh/fit/apigetaway/ApiGetAwayApplication.java
@@ -0,0 +1,13 @@
+package vn.edu.iuh.fit.apigetaway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGetAwayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGetAwayApplication.class, args);
+    }
+
+}
---
+++ APIGetAway1/src/main/java/vn/edu/iuh/fit/apigetaway/controller/APIController.java
@@ -0,0 +1,29 @@
+package vn.edu.iuh.fit.apigetaway.controller;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import vn.edu.iuh.fit.apigetaway.service.APIGetAwayService;
+
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
+public class APIController {
+    @Autowired
+    private APIGetAwayService apiGetAwayService;
+
+    @GetMapping("/listUser")
+    public List<Map<String, Object>> getAllUsers(){
+        List<Map<String, Object>> userList = apiGetAwayService.getAllUser();
+        return userList;
+    }
+
+    @GetMapping("/listProduct")
+    public List<Map<String, Object>> getAllProduct(){
+        List<Map<String, Object>> productList = apiGetAwayService.getAllProduct();
+        return productList;
+    }
+}
